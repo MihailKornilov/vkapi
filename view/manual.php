40db3f42dc8571844b5724276a2202bc50176740
miha <mihan_k@mail.ru>
2016-02-17 03:17:47 +0300

    manual ..

--- view/manual.php
+++ view/manual.php
@@ -1,7 +1,8 @@
 <?php
 function _manual() {
-	if($id = _num(@$_GET['page_id']))
-		return _manual_page($id);
+	if(@$_GET['d'] == 'part')
+		return _manual_path();
+
 	return _manual_main();
 }
 
@@ -15,6 +16,25 @@ function _manualPart($id=false, $i='name') {
 				ORDER BY `sort`";
 		if($arr = query_arr($sql, GLOBAL_MYSQL_CONNECT))
 			xcache_set($key, $arr, 86400);
+	}
+
+	if($i == 'menu') {//меню справа в списке разделов
+		if(!$id)
+			$id = key($arr);
+
+		$send = '';
+		foreach($arr as $r) {
+			$sel = $id == $r['id'] ? ' class="sel"' : '';
+			$send .= '<a'.$sel.' href="'.URL.'&p=manual&d=part&part_id='.$r['id'].'">'.$r['name'].'</a>';
+		}
+		return '<div class="rightLink">'.$send.'</div>';
+	}
+
+	if($id == 'default') {//получение значения по умолчанию, либо если соответствует GET
+		$id = _num(@$_GET['part_id']);
+		if(isset($arr[$id]))
+			return $id;
+		return key($arr);
 	}
 
 	if($id == 'all')
@@ -55,9 +75,9 @@ function _manualPartSub($id=false, $i='name') {
 function _manualMenu() {//разделы основного меню
 	$menu = array(
 		'main' => 'Мануал',
-		'part' => 'Разделы',
-		'new' => 'Нововведения',
-		'dialog' => 'Обсуждения'
+		'part' => 'Разделы'
+//		'new' => 'Нововведения',
+//		'dialog' => 'Обсуждения'
 	);
 
 	if(empty($_GET['d']) || !isset($menu[@$_GET['d']]))
@@ -76,15 +96,6 @@ function _manualMenu() {//разделы основного меню
 	'<div id="_menu">'.
 		$link.
 		'<a class="back" href="'.URL.'">'._app('name').'</a>'.
-	'</div>';
-}
-function _manual_path($part_id=0,$part_sub_id=0) {//путь
-	return
-	'<div class="path">'.
-		'<a href="'.URL.'">'._app('name').'</a>'.
-		' » '.
-		($part_id ? '<a href="'.URL.'&p=manual">Мануал</a> » '._manualPart($part_id) : 'Мануал').
-		($part_sub_id ? ' » '._manualPartSub($part_sub_id) : '').
 	'</div>';
 }
 function _manual_main() {//главная страница
@@ -111,7 +122,7 @@ function _manual_part() {//главная страница
 	foreach($spisok as $r) {
 		$send .=
 			'<div class="part">'.
-				'<a class="part-head">'.
+				'<a class="part-head" href="'.URL.'&p=manual&d=part&part_id='.$r['id'].'">'.
 					'<span class="name">'.$r['name'].'</span>'.
 				'</a>'.
 				'<div class="part-sub">'.$r['sub'].'</div>'.
@@ -158,6 +169,49 @@ function _manual_part_js() {
 	'</script>';
 }
 
+function _manual_path() {//путь
+	$left = _manual_content();
+	$part_id = _manualPart('default');
+
+	return
+	_manualMenu().
+	'<div id="manual-part">'.
+		'<table class="tabLR">'.
+			'<tr><td id="left">'.$left.
+				'<td class="right">'._manualPart($part_id, 'menu').
+		'</table>'.
+	'</div>';
+}
+function _manual_content() {//содержание с левой стороны
+	$part_id = _manualPart('default');
+
+	if($id = _num(@$_GET['page_id']))
+		return _manual_page($id);
+
+
+	return
+	'<h1>'._manualPart($part_id).'</h1>'.
+	_manual_page_spisok($part_id);
+}
+function _manual_page_spisok($part_id) {
+	$sql = "SELECT *
+			FROM `_manual`
+			WHERE `part_id`=".$part_id."
+			ORDER BY `sort`";
+	if(!$spisok = query_arr($sql, GLOBAL_MYSQL_CONNECT))
+		return 'Страниц нет.';
+
+	$end = end($spisok);
+	$send = '';
+	foreach($spisok as $r) {
+		$send .=
+			'<div class="page-unit'.($r['id'] == $end['id'] ? ' noborder' : '').'">'.
+				'<a href="'.URL.'&p=manual&d=part&page_id='.$r['id'].'">'.$r['name'].'</a>'.
+			'</div>';
+	}
+
+	return $send;
+}
 function _manual_page($id) {//отображение страницы мануала
 	$sql = "SELECT *
 			FROM `_manual`
@@ -166,6 +220,6 @@ function _manual_page($id) {//отображение страницы мануала
 		return _err('Страницы не существует.');
 
 	return
-	_manual_path($r['part_id'], $r['part_sub_id']).
-	$r['name'];
+	'<h1>'.$r['name'].'</h1>'.
+	'<h2>'.$r['content'].'</h2>';
 }
